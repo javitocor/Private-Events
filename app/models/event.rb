f7856b22f7d632567922206c308852cae2843f6e
javitocor <javitocor@gmail.com>
2020-05-19 18:09:59 +0200

    Modified foreign keys in associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
-  has_many :attendances, foreign_key: :attendee_id
+  has_many :attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :attendances, source: :attendee
 
   scope :past, -> { where('date < ?', Date.today) }

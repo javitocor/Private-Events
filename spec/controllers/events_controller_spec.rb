363da2af18aa41a6df7cb1c8381799c9aa0df1c3
javitocor <javitocor@gmail.com>
2020-05-21 12:49:04 +0200

    Added tests to events controller

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -15,10 +15,6 @@ RSpec.describe EventsController, type: :controller do
       get :show, params: { id: '1'}
       expect(response).to be_successful
     end
-    it 'GET #new' do 
-      @current_user = @user
-      expect(get("/events/new")).to route_to("events#new")
-    end
   end
   context 'Create an event' do 
     it 'should create a new event' do 
@@ -27,46 +23,79 @@ RSpec.describe EventsController, type: :controller do
       expect(@event.save).to eq(true)
     end
   end
-  
-end
-
-RSpec.describe EventsController, type: :feature do
   context 'Attend an event' do 
-    it 'should create and attendance for an event' do 
+    it 'should create an attendance for an event' do 
       @user = User.new(id:'1', name:'peter', email:'peter@gmail.com').save
       @event = Event.new(id:'1', title: "Party", description: "party super cool", location: "gotham", date: "2023-05-19 01:00:00", creator_id:'1').save
       @attend = Attendance.new(attendee_id: '1', attended_event_id: '1')
       expect(@attend.save).to eq(true)
     end
-  end
+  end  
 end
 
-RSpec.describe 'create new event and redirect to event show page', type: :feature do
-  let(:user) { User.create(name: 'Peter', email: 'peter@example.com') }
-  let(:event) { Event.create(title: "Party", description: "party super cool", location: "gotham", date: "2023-05-19 01:00:00", creator_id:'1') }
+RSpec.describe 'Event Management', type: :feature do
+  let(:user) { User.create(id:'1', name: 'Peter', email: 'peter@example.com') }
+  let(:event) { Event.create(id:'1', title: "Party", description: "party super cool", location: "gotham", date: "2023-05-19 01:00:00", creator_id:'1') }
+  before(:each) do 
+    visit root_path
+    fill_in 'session_name', with: user.name 
+    click_button 'Log In'
+  end
 
   scenario 'create event' do
-    @current_user = user.id
     visit new_event_path
-    fill_in 'event_title', with: event.title
-    fill_in 'event_description', with: event.description
-    fill_in 'event_location', with: event.location
-    fill_in 'event_date', with: event.date
+    fill_in 'event_title', with: "Party"
+    fill_in 'event_description', with: "party super cool"
+    fill_in 'event_location', with: "gotham"
+    select '2020',  from: 'event_date_1i'
+    select 'January',  from: 'event_date_2i'
+    select '1', from: 'event_date_3i'
+    #select '00', from: 'event_date_4i'
+    #select '15', from: 'event_date_5i'
     click_button 'Create Event'
     sleep(3)
-    expect(page).to have_content(event.title)    
+    expect(page).to have_content("Party")    
   end
-end
-
-RSpec.describe 'attend an event', type: :feature do
-  let(:user) { User.create(name: 'Peter', email: 'peter@example.com') }
-  let(:event) { Event.create(title: "Party", description: "party super cool", location: "gotham", date: "2023-05-19 01:00:00", creator_id:'1') }
 
   scenario 'attend event' do
     visit event_path(event)
     click_button 'Attend'
     sleep(3)
-    expect(page).to have_content('Peter')    
+    expect(page).to have_content('Past Hosted Events')    
+  end
+
+  scenario 'Get new page' do 
+    visit new_event_path
+    expect(page).to have_content("Create Event")
+  end
+end
+
+RSpec.describe 'Create event fields', type: :feature do
+  let(:user) { User.create(id:'1', name: 'Peter', email: 'peter@example.com') }
+  let(:event) { Event.create(id:'1', title: "Party", description: "party super cool", location: "gotham", date: "2023-05-19 01:00:00", creator_id:'1') }
+  before(:each) do 
+    visit root_path
+    fill_in 'session_name', with: user.name 
+    click_button 'Log In'
+  end
+
+  describe 'fields in Create event page' do
+    it 'title field in Create event page' do
+      visit new_event_path
+      expect(page).to have_content 'Title'
+    end
+    it 'Description field in Create event page' do
+      visit new_event_path
+      expect(page).to have_content 'Description'
+    end
+    it 'Location field in Create event page' do
+      visit new_event_path
+      expect(page).to have_content 'Location'
+    end
+    it 'Date field in Create event page' do
+      visit new_event_path
+      expect(page).to have_content 'Date'
+    end
   end
 end
 

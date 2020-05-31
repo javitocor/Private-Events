612d52c8cf16b38c6b7af4097e37850b8a110437
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-05-31 18:02:51 +0530

    removed source from has_many assoc

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,7 +6,7 @@ class Event < ApplicationRecord
 
   belongs_to :creator, class_name: 'User'
   has_many :attendances, foreign_key: :attended_event_id
-  has_many :attendees, through: :attendances, source: :attendee
+  has_many :attendees, through: :attendances
 
   scope :past, -> { where('date < ?', Date.today) }
   scope :upcoming, -> { where('date > ?', Date.today) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
   has_many :attendances, foreign_key: :attendee_id
-  has_many :attended_events, through: :attendances, source: :attended_event
+  has_many :attended_events, through: :attendances
 
   def past_events
     attended_events.where('date < ?', Date.today)
